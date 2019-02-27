c02053a88c398e67255f0b05ad50e6c1cc6dada2
darling-trace-1821 <kmanrique93@gmail.com>
2019-02-27 01:44:30 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-str = "Squawk!"
-def parrot(str)
-  puts str
-  return str
+
+def parrot
+  puts "Squawk!"
+  return "Squawk!"
 end 
