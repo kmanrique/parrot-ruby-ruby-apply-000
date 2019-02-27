d6db6951940a42f8853e252411e4d26200b921f9
darling-trace-1821 <kmanrique93@gmail.com>
2019-02-27 01:58:33 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,13 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-str = "Squawk!"
-def parrot(str)
-  puts "#{str}"
-  "#{str}"
-end
-
-#def parrot(string="Squawk!")
- # puts "#{string}"
-  #"#{string}"
+#str = "Squawk!"
+#def parrot(str)
+ # puts "#{str}"
+  #"#{str}"
 #end
+
+def parrot(string="Squawk!")
+ puts "#{string}"
+ "#{string}"
+end
